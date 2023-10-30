b88219f29ad7058f726fff5c3cfcfe1ff3c2d803
marina <100472115@alumnos.uc3m.es>
2023-10-30 18:48:34 +0100

    test

---
+++ utest/test_general.cpp
@@ -0,0 +1,15 @@
+//
+// Created by marina on 30/10/23.
+//
+#include "gtest/gtest.h"
+#include "fluid/fluid.cpp"
+
+
+TEST(unitarias, uno){
+
+  EXPECT_EQ(1,1);
+}
+TEST(unitarias, dos){
+  EXPECT_EQ(3,3);
+
+}
