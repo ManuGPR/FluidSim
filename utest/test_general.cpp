d76a77d74674d7c45c31fa2e1626e85a8f779d86
marina <100472115@alumnos.uc3m.es>
2023-11-02 18:12:52 +0100

    test 2

--- utest/test_general.cpp
+++ utest/test_general.cpp
@@ -4,6 +4,7 @@
 #include "gtest/gtest.h"
 #include "sim/calculos.hpp"
 #include "sim/simulacion.cpp"
+#include <fstream>
 
 TEST(unitarias, uno){
   EXPECT_EQ(1,1);
@@ -11,4 +12,14 @@ TEST(unitarias, uno){
 TEST(unitarias, dos){
   EXPECT_EQ(3,3);
 }
-
+/*
+TEST(calculos,uno){
+ const vector<string> vec = {"1", "../small.fld","../out.fld"};
+ simulation(vec);
+ ifstream archivo1;
+ ifstream archivo2;
+ archivo1.open("../boundint-base-5.trz", ios::binary);
+ archivo2.open( "../out.fld", ios::binary);
+ ASSERT_EQ(archivo2, archivo1);
+}
+*/
